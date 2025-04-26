62ab14760a3b95b38341b1049f533ff8053bda1d
Asmaidbaba <asmaeidbaba5@gmail.com>
2025-04-27 00:12:57 +0100

    Déploiement Azure avec Terraform

---
+++ main.tf
@@ -0,0 +1,63 @@
+provider "azurerm" {
+  features {}
+  subscription_id = "c61a92b8-b74e-4136-8fd7-241ed45e27be"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "asma-projet-group"
+  location = "East US"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "asma-projet-vnet"
+  address_space       = ["10.1.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "asma-projet-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.1.2.0/24"]
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "asma-projet-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "asma-projet-vm"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s" 
+  admin_username      = "asma"
+
+  admin_password = "Asma@12345678"
+
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  disable_password_authentication = false
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts"
+    version   = "latest"
+  }
+}
